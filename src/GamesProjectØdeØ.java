034db952616f5fb04732e3e8ed793963f8aa5574
LauraStorm <lbs2rm@hotmail.com>
2021-09-16 09:45:10 +0200

    Commit d. 16/9 9:44

--- src/GamesProjectØdeØ.java
+++ src/GamesProjectØdeØ.java
@@ -11,14 +11,29 @@ public class GamesProjectØdeØ {
         // 10 options for user to choose:
         String [] options = {"1. Tent", "2. Madras", "3. Music", "4. A Friend", "5. Matches", "6. Knife", "7. Book", "8. A pet", "9. Food", "10. coffee"};
 
-        //print option til user:
+        //print option til user
         for (String option : options) {
             System.out.println(option);
         }
 
-        //User choose one option:
+        //Get 10 user inputs - they choose one option each
         System.out.println("Type the number of your choice:");
-        int userOption1 = scanner.nextInt();
+        //Denne kan ændres til en while loop - hvis der er tid.****
+        //while gør at vi kan få 10 input også hvis man svarer forker*****
+
+        for (int i = 1; i <= options.length; i++) {
+            int userInputs = scanner.nextInt();
+            System.out.println(i);
+
+            if ( userInputs > options.length){
+                System.out.println("Your choice is not available");
+            }
+        }
+
+        //Get userInputs in an array
+
+
+
 
 
 
