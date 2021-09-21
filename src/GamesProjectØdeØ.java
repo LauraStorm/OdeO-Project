a2ff6042537f7b65881f4a6c8a18a97d2dd40909
LauraStorm <lbs2rm@hotmail.com>
2021-09-21 17:51:01 +0200

    Commit d. 21/9

--- src/GamesProjectØdeØ.java
+++ src/GamesProjectØdeØ.java
@@ -4,27 +4,27 @@ public class GamesProjectØdeØ {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //INTRO for user:
-        System.out.println("Imagine that you are on a desert Island 🏝  and you can only bring 1 thing with you");
-        System.out.println("I made a list. Which of these items would you choose?");
+        //INTRO for user
+        System.out.println("Imagine that you are on a desert Island 🏝  and you can only bring 1 thing with you!");
+        System.out.println("Which of the following items would you choose to bring with you?\n");
 
-        // 10 options for user to choose:
-        String [] options = {"1. Tent", "2. Madras", "3. Music", "4. A Friend", "5. Matches", "6. Knife", "7. Book", "8. A pet", "9. Food", "10. coffee"};
+
+        // 10 options for user to choose
+        String [] options = {"1. Your bed", "2. A TV", "3. Music", "4. A Friend", "5. Coffee machine", "6. Alcohol", "7. A pet", "8. Your favorite dish", "9. Your wardrobe ", "10. Your mom"};
         int[] optionIndex = new int[10];
 
-        //print option til user
+
+        //Print option for user
         for (String option : options) {
             System.out.println(option);
         }
 
-        //Get 10 user inputs - they choose one option each
-        System.out.println("Type the number of your choice:");
-        //Denne kan ændres til en while loop - hvis der er tid.****
-        //while gør at vi kan få 10 input også hvis man svarer forker*****
 
+        //Get 10 user inputs - they choose one option each
         for (int i = 1; i <= options.length; i++) {
+            System.out.println("Type the number of your choice:");
             int userInputs = scanner.nextInt();
-            System.out.println(i);
+            System.out.println(i + ". Answer\n--------------------------------");
 
             optionIndex[userInputs-1] = optionIndex[userInputs-1] + 1;
 
@@ -33,12 +33,14 @@ public class GamesProjectØdeØ {
             }
         }
 
-       // User input in an array
+
+       // Result from user input
         for (int option : optionIndex) {
             System.out.print(option + ", ");
         }
 
-        //Find most popular option
+
+        //Find the most popular option
         int maximum = optionIndex[0];
         int mostChosenElement = 0;
 
@@ -51,7 +53,7 @@ public class GamesProjectØdeØ {
             }
         }
 
-        //If there is more than one items there is popular (has the same votes) - show both/all element there has the same amount of votes
+        //If there is more than one item there is popular (has the same amount of votes) - show both/all element there has the same amount of votes
         String moreThan2ChosenElement = "";
         int mostChosenValuesIndex = optionIndex[mostChosenElement];
 
@@ -60,16 +62,8 @@ public class GamesProjectØdeØ {
                 moreThan2ChosenElement += options[i] + ", ";
             }
         }
-        System.out.println("\n" + moreThan2ChosenElement + "is the most frequent item(s) that was chosen");
-
-
-
-
-
-
-
-
-
+        System.out.println("\n" + moreThan2ChosenElement + "is the most frequent item(s) that the users would bring to a desert island 🏝 ");
 
     }
+
 }
